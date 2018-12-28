d8b5367458573a67a6b373aa26fdbcb0b9ecea66
kgame <kgamecarter@gmail.com>
2018-12-28 11:47:10 +0800

    create crapto1.dart

---
+++ lib/services/crapto1.dart
@@ -0,0 +1,15 @@
+import 'dart:typed_data';
+
+class Crypto1State {
+  int odd;
+  int even;
+}
+
+class Crypto1 {
+  static const int LF_POLY_ODD = 0x29CE5C;
+  static const int LF_POLY_EVEN = 0x870804;
+
+  Crypto1State state;
+
+  
+}
