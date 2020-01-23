3179ad2b419e67b2701fec698b56b0a1263d7373
kgame <kgamecarter@gmail.com>
2020-01-23 13:49:43 +0800

    update depandency

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/material.dart';
 import 'services/settings.dart';
 import 'routes.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Settings().load();
   Routes();
 }
